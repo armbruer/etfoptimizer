ab8b11274da5e85be5e4bf724f579ded20424905
Eric Armbruster <eric.armbruster@tum.de>
2021-03-11 16:55:48 +0100

    WIP: Add selenium to handle javascript

--- ETFOptimizer/spiders/justetf.py
+++ ETFOptimizer/spiders/justetf.py
@@ -1,39 +1,94 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy import Request
+from selenium import webdriver
+from scrapy.selector import Selector
+import time
+import logging
 
 from ETFOptimizer.items import EtfItem
 
 
 def get_table_values(selector, table_size):
     values = []
-    for i in range(1, table_size+1):
-        values.append(selector.xpath('*[" + str(i) + "]/td[2]/text()').getall())
+    for i in range(1, table_size + 1):
+        values.append(selector.xpath('*[' + str(i) + ']/td[2]/text()').getall())
 
     return values
 
 
-class JustetfSpider(CrawlSpider):
+class JustetfSpider(scrapy.Spider):
     name = 'justetf'
     allowed_domains = ['justetf.com']
     start_urls = ['https://justetf.com/de-en/find-etf.html']
 
-    rules = (
-        Rule(LinkExtractor(allow=r'etf-profile\.html'), callback='parse_item', follow=True),
-    )
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
+        #self.driver = webdriver.Chrome()
+
+        # Use headless option to not open a new browser window
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        desired_capabilities = options.to_capabilities()
+        self.driver = webdriver.Chrome(desired_capabilities=desired_capabilities)
+        self.driver.implicitly_wait(10)
+
+        self.link_extractor = LinkExtractor(allow=r'etf-profile.html')
+
+    def extract_links(self, response):
+        for link in self.link_extractor.extract_links(response):
+            yield Request(link.url, callback=self.parse_item)
 
     def parse(self, response, **kwargs):
-        pass
+        logging.info("Begin parsing ...")
+        # load first page second time, this time through selenium
+        self.driver.get(response.url)
+
+        # cookie settings: allow selection
+        allow_selection_button = self.driver.find_element_by_xpath('//a[@id="CybotCookiebotDialogBodyLevel'
+                                                                   'ButtonLevelOptinAllowallSelection"]')
+        allow_selection_button.click()
+        #time.sleep(0.5)
+
+        # reduce the number of page loads required for better throughput, and fewer requests
+        #show_rows_button = self.driver.find_element_by_xpath('//a/span[contains(text(), "Show 25 rows")]')
+        #show_rows_button.click()
+        #select_rows_button = self.driver.find_element_by_xpath('//a[contains(text(), "100 rows")]')
+        #select_rows_button.click()
+
+        time.sleep(2.5)
+        #logging.info(self.driver.page_source)
+        r = Selector(text=self.driver.page_source).response
+        logging.info(r)
+        self.extract_links(r)
+        pagenum = 1
+        logging.info(f"Extracted etfs from page {pagenum}")
+
+        while True:
+            next_page = self.driver.find_element_by_xpath('//a[@id="etfsTable_next"]')
+
+            try:
+                next_page.click()
+                # wait for javascript to load in selenium
+                time.sleep(2.5)
+                self.extract_links(Selector(text=self.driver.page_source).response)
+                pagenum += 1
+                logging.info(f"Extracted etfs from page {pagenum}")
+            except:
+                break
+        self.driver.close()
+        logging.info("Finished parsing")
 
     def parse_item(self, response):
         # TODO nonstrings should be parsed properly
         isin_parent = response.xpath('//span[@class="vallabel" and .="ISIN"]/..')
         name = isin_parent.xpath('../*[1]/text()').getall()
+        logging.info(f"Parsing ETF '{name}'")
+
         isin = isin_parent.xpath('*[2]/text()').getall()
         wkn = isin_parent.xpath('*[4]/text()').getall()
 
         item = EtfItem(name=name, isin=isin, wkn=wkn)
-
         item['ter'] = response.xpath('//div[@class="h5" and contains(text(), "Fees")]/../div[2]/div/div[1]/div['
                                      '1]/text()').getall()
 
@@ -88,4 +143,4 @@ class JustetfSpider(CrawlSpider):
         item['securities_lending'] = values[3]
         item['securities_lending_counterparty'] = values[4]
 
-        return item
+        yield item
