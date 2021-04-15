e1645db07acd84f43765e546f4d8a7650bfe6efb
BiRuacc <rubenbachmann@gmx.net>
2021-04-15 21:24:00 +0200

    Finished most important fixes.

--- etf_history.py
+++ etf_history.py
@@ -4,18 +4,16 @@ import os
 import pandas
 
 from datetime import datetime
-from db.dbconnector import db_connect, create_table
-from sqlalchemy.orm import sessionmaker
-
+from db.dbconnector import create_table, db_connect
 from db.models import EtfHistory
+from sqlalchemy.orm import sessionmaker
 
 
 def get_isin_dict():  # todo file parameter
     isin_dict = {}
 
-    with open(os.path.dirname(os.path.join(os.path.realpath(__file__), 'isin.csv')), 'r') as csv_file:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'isin.csv'), 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
-        header = next(csv_reader)
 
         for row in csv_reader:
             isin_dict[row[1]] = row[0]
@@ -23,11 +21,11 @@ def get_isin_dict():  # todo file parameter
     return isin_dict
 
 
-def get_dynamic_data(session):  # todo file parameter
+def get_etf_history(session):  # todo file parameter
     isin_dict = get_isin_dict()
 
-    dynamic_data = pandas.read_csv(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'etf_history.csv'), sep = ';')
-    header = dynamic_data.columns.to_list()
+    etf_history = pandas.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'etf_history.csv'), sep = ';')
+    header = etf_history.columns.to_list()
 
     isin = ''
 
@@ -35,28 +33,28 @@ def get_dynamic_data(session):  # todo file parameter
     price_index = 0.0
     return_index = 0.0
 
-    for i in range(0, len(dynamic_data)):
-        date = datetime.strptime(dynamic_data.iloc[i, 0], '%d.%m.%Y').date()
+    for i in range(0, len(etf_history)):
+        datapoint_date = datetime.strptime(etf_history.iloc[i, 0], '%d.%m.%Y').date()
 
-        for j in range(1, len(dynamic_data.columns)):
+        for j in range(1, len(etf_history.columns)):
             if j % 3 == 1:
                 isin = isin_dict[header[j]]
-                price = float(dynamic_data.iloc[i, j])
+                price = float(etf_history.iloc[i, j])
             elif j % 3 == 2:
-                price_index = float(dynamic_data.iloc[i, j])
+                price_index = float(etf_history.iloc[i, j])
             else:
-                return_index = float(dynamic_data.iloc[i, j])
+                return_index = float(etf_history.iloc[i, j])
 
                 try:
                     database_data = session.query(EtfHistory)
-                    exists = database_data.filter_by(isin=data.isin, datapoint_date=data.datapoint_date).first() is not None  # todo pls fix
+                    exists = database_data.filter_by(isin=isin, datapoint_date=datapoint_date).first() is not None
                     if exists:
                         logging.warning(f'Updated values are not reflected in database.')
                     else:
                         data = EtfHistory()
 
                         data.isin = isin
-                        data.datapoint_date = date
+                        data.datapoint_date = datapoint_date
 
                         data.price = price
                         data.price_index = price_index
@@ -70,16 +68,16 @@ def get_dynamic_data(session):  # todo file parameter
                     raise
 
 
-def create_etf_history_database():
+def create_database():
     engine = db_connect()
     create_table(engine)
 
     Session = sessionmaker(engine)
     session = Session()
 
-    get_dynamic_data(session)
+    get_etf_history(session)
 
     session.close()
 
 
-create_etf_history_database()  # todo remove add commands to run_crawlers.py
+create_database() # Add to run_crawlers.py
