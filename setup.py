3f63d7185573b4b0444fcc2ac2326a80616f02b2
Eric Armbruster <eric.armbruster@tum.de>
2021-04-20 21:48:47 +0200

    Set fixed minimum and maximum package versions for python dependencies to ensure project stability.

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires= [
-        'Click>=7.1.2',
-        'scrapy>=2.5.0',
+        'Click>=7.1.2,<7.2',
+        'scrapy>=2.5.0,<2.6',
         'selenium>=3.141.0',
-        'psycopg2>=2.8.6',
-        'SQLAlchemy>=1.4.9',
-        'pandas>=1.2.4',
-        'requests>=2.25.1',
-        'dash>=1.20.0',
-        'dash-bootstrap-components>=0.12.0',
+        'psycopg2>=2.8.6,<2.9',
+        'SQLAlchemy>=1.4.9,<1.5',
+        'pandas>=1.2.4,<1.3',
+        'requests>=2.25.1,<2.26',
+        'dash>=1.20.0,<1.21',
+        'dash-bootstrap-components>=0.12.0,<0.13',
     ],
     entry_points='''
         [console_scripts]
