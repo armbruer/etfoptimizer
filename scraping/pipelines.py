1366c030772e7bf12ac1af8555ac5e068da3f54b
Eric Armbruster <eric.armbruster@tum.de>
2021-04-24 09:30:25 +0200

    Remove unused code.

--- scraping/pipelines.py
+++ scraping/pipelines.py
@@ -5,15 +5,9 @@
 
 
 import logging
-
-# useful for handling different item types with a single interface
-from scrapy.exceptions import DropItem
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import sessionmaker
-
-from scraping.items import l2v
 from db.dbconnector import db_connect, create_table
-from db.dbmodels import Etf, EtfCategory, IsinCategory
+from db.dbmodels import Etf
 
 
 class EtfPipeline:
@@ -37,7 +31,9 @@ class EtfPipeline:
             etf_current = self.session.query(Etf)
             exists = etf_current.filter_by(isin=etf.isin).first() is not None
             if exists:
-                logging.warning(f'Updated values are not reflected in database.')
+                logging.warning(f'Updated values are not reflected in database for values scraped from justetf.com. '
+                                f'Please delete this table if you want to get fresh values into the database and '
+                                f'ensure extraetf.com is first scraped.')
             else:
                 self.session.add(etf)
                 self.session.commit()
@@ -48,40 +44,3 @@ class EtfPipeline:
 
         return item
 
-
-class EtfCategoryPipeline:
-
-    def __init__(self):
-        engine = db_connect()
-        create_table(engine)
-        self.Session = sessionmaker(bind=engine)
-
-    def open_spider(self, spider):
-        self.session = self.Session()
-
-    def close_spider(self, spider):
-        self.session.close()
-
-    def process_item(self, item, spider):
-        category = l2v(item, 'category')
-        isin = l2v(item, 'isin')
-
-        if category is None:
-            raise DropItem(f"Category is none for {isin}")
-
-        if self.session.query(Etf).filter_by(isin=isin).first() is not None:
-            category_row = self.session.query(EtfCategory).filter_by(category=category).first()
-            if category_row is not None:
-                try:
-                    isin_category = IsinCategory(etf_isin=isin, category_id=category_row.id)
-                    self.session.add(isin_category)
-                    self.session.commit()
-                    return item
-                except SQLAlchemyError as e:
-                    logging.error(e.args)
-                    self.session.rollback()
-                    raise DropItem(f"Could not save category for {isin} due to SQL error!")
-            else:
-                raise DropItem(f"Could not save category for {isin} as category {category} is unknown!")
-        else:
-            raise DropItem(f"Could not save category for {isin} as no ETF with given ISIN exists!")
