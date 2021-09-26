aef0af799615eb1e25910403a6d5de000b059ab3
Eric Armbruster <eric.armbruster@tum.de>
2021-09-26 10:29:14 +0200

    Add sentence about docs

--- db/__init__.py
+++ db/__init__.py
@@ -24,7 +24,7 @@ if config.get_value('database-uri', 'username') == "<username>" or config.get_va
                                                                                     'password') == "<password>":
     click.echo("Please make sure to configure your database connection in etfoptimizer.ini first. At minimum you need "
                "to replace values of the format <...> with the expected values. You should find the file in "
-               "~/.config/etfoptimizer on Linux and in C::\\Users\\<windows user>\\AppData\\Local\\etfoptimizer\\Config")
+               "~/.config/etfoptimizer on Linux and in C::\\Users\\<windows user>\\AppData\\Local\\etfoptimizer\\Config. If you need further assistance, please read documentation.pdf")
     sys.exit(1)
 
 sql_engine = create_engine(uri)
